25268abf07d96ad45a66e325671c859a232faa6c
Fede Isas <fedeisas@hotmail.com>
2011-10-17 11:35:48 -0300

    IdeaScale Site

---
+++ third_party/site_update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ideascale
+wget --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows --domains ideascale.com --no-parent custom.ideascale.com/
